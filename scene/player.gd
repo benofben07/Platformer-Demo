1a5b80f957de0131812a49168f677b5f128855c2
benofben07 <benofben07@gmail.com>
2024-05-07 04:42:48 +0200

    Add double jump

--- scene/player.gd
+++ scene/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var jumping: bool = false
+var double_jumping: bool = false
 var landing: bool = false
 var was_in_air: bool = false
 
@@ -19,12 +20,20 @@ func _physics_process(delta):
 			landing = true
 			was_in_air = false
 		jumping = false
+		double_jumping = false
 
 	var jumping_pressed = Input.is_action_just_pressed("jump")
+	var double_jumping_pressed: bool = false
 	# Handle jump.
-	if jumping_pressed and is_on_floor() and not landing:
-		velocity.y = JUMP_VELOCITY
-		jumping = true
+	if jumping_pressed and not landing:
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jumping = true
+		elif not is_on_floor() and not double_jumping:
+			velocity.y = JUMP_VELOCITY
+			double_jumping_pressed = true
+			jumping = true
+			double_jumping = true
 		
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction and not landing:
@@ -32,32 +41,35 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	update_sprite_animation(direction, jumping_pressed)
+	update_sprite_animation(direction, jumping_pressed, double_jumping_pressed)
 	flip_character_sprite_if_needed(direction)
 	move_and_slide()
 	
 	if not is_on_floor():
 		was_in_air = true
 	
-func update_sprite_animation(direction: float, jumping_pressed: bool):
+func update_sprite_animation(direction: float, jumping_pressed: bool, double_jumping_pressed:bool):
 	if not is_on_floor():
-		if not jumping:
+		if double_jumping_pressed:
+			animated_sprite.play("jump_start")
+		elif not jumping:
 			animated_sprite.play("falling")
+		
 		return
 		
 	if landing:
 		animated_sprite.play("jump_landing")
 		return
 		
-	if jumping_pressed and not landing:
+	if jumping_pressed:
 		animated_sprite.play("jump_start")
 		return
 
-	if not direction and not landing:
+	if not direction:
 		animated_sprite.play("idle")
 		return
 
-	if direction and not landing:
+	if direction:
 		animated_sprite.play("run")
 		return
 
