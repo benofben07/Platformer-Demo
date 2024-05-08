8669a35428a7e3188c259a42d63f08ba487a9b54
benofben07 <benofben07@gmail.com>
2024-05-08 16:43:24 +0200

    Add dashing

--- scene/player.gd
+++ scene/player.gd
@@ -1,60 +1,126 @@
 extends CharacterBody2D
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var dashing_time = 0.4
+@export var dashing_speed = 450.0
+@export var jump_velocity = -350.0
+@export var double_jump_velocity = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var dash_timer: Timer = $DashTimer
+
 var jumping: bool = false
 var double_jumping: bool = false
 var landing: bool = false
+var dashing: bool = false
 var was_in_air: bool = false
+var was_dashing: bool = false
+var last_facing_direction: float = 1.0
+var dashing_direction: float = 1.0
+var dash_ending_animation_in_progress: bool = false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
+		# gravity
 		velocity.y += gravity * delta
+		# dashing isn't in progress while in the air
+		dashing = false
 	else:
-		if jumping or was_in_air:
+		# landing
+		if was_in_air:
 			landing = true
 			was_in_air = false
+			
 		jumping = false
 		double_jumping = false
 
+	# dashing stops at this frame
+	if dashing and dash_timer.is_stopped():
+		dashing = false
+
 	var jumping_pressed = Input.is_action_just_pressed("jump")
+	var dashing_pressed = Input.is_action_just_pressed("dash")
 	var double_jumping_pressed: bool = false
-	# Handle jump.
-	if jumping_pressed and not landing:
+	
+	# jump
+	if jumping_pressed and not landing and not dashing:
+		# simple jump
 		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jump_velocity
 			jumping = true
+			dashing = false
+			was_dashing = false
+		# double jump
 		elif not is_on_floor() and not double_jumping:
-			velocity.y = JUMP_VELOCITY
+			velocity.y = jump_velocity
 			double_jumping_pressed = true
 			jumping = true
 			double_jumping = true
 		
+	# movement
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction and not landing:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	update_sprite_animation(direction, jumping_pressed, double_jumping_pressed)
+	# dont smooth with controller movement
+	if direction < 0:
+		direction = -1
+	elif direction > 0:
+		direction = 1
+	
+	# dashing in progress
+	# dont let change direction while dashing
+	if dashing:
+		velocity.x = last_facing_direction * dashing_speed
+	
+	# dashing
+	if dashing_pressed and not dashing and not jumping:
+		dash_timer.start(dashing_time)
+		dashing = true
+		velocity.x = direction * dashing_speed
+		landing = false
+	
+	# movement
+	if direction and not landing and not dashing:
+		velocity.x = direction * speed
+		dash_ending_animation_in_progress = false
+	elif not dashing:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	update_sprite_animation(direction, jumping_pressed, double_jumping_pressed, dashing_pressed)
 	flip_character_sprite_if_needed(direction)
 	move_and_slide()
 	
+	# setting up variables for next frame
 	if not is_on_floor():
 		was_in_air = true
+		
+	if direction and not dashing:
+		last_facing_direction = direction
 	
-func update_sprite_animation(direction: float, jumping_pressed: bool, double_jumping_pressed:bool):
+	was_dashing = dashing
+	
+func update_sprite_animation(direction: float, jumping_pressed: bool, double_jumping_pressed: bool, dashing_pressed: bool):
 	if not is_on_floor():
 		if double_jumping_pressed:
 			animated_sprite.play("jump_start")
 		elif not jumping:
 			animated_sprite.play("falling")
 		
+		return
+	
+	if dashing_pressed and not was_dashing:
+		animated_sprite.play("dash_start")
+		return
+	
+	# lock dashing animation
+	if dashing:
+		return
+	# dashing finished
+	elif not dashing and was_dashing:
+		animated_sprite.play("dash_end")
+		dash_ending_animation_in_progress = true
 		return
 		
 	if landing:
@@ -65,7 +131,7 @@ func update_sprite_animation(direction: float, jumping_pressed: bool, double_jum
 		animated_sprite.play("jump_start")
 		return
 
-	if not direction:
+	if not direction and not dash_ending_animation_in_progress:
 		animated_sprite.play("idle")
 		return
 
@@ -74,7 +140,9 @@ func update_sprite_animation(direction: float, jumping_pressed: bool, double_jum
 		return
 
 func flip_character_sprite_if_needed(direction: float):
-	if direction < 0:
+	if dashing:
+		return
+	elif direction < 0:
 		animated_sprite.flip_h = true
 	elif direction > 0:
 		animated_sprite.flip_h = false
@@ -85,7 +153,11 @@ func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite.animation == "jump_in_air":
 		animated_sprite.play("falling")
 	if animated_sprite.animation == "jump_landing":
-		landing = false				
+		landing = false
+	if animated_sprite.animation == "dash_start":
+		animated_sprite.play("dash_loop")
+	if animated_sprite.animation == "dash_end":
+		dash_ending_animation_in_progress = false
 
 func bool_to_str(b: bool):
 	return "True" if b else "False"
